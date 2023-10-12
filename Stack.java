3e9053d9c88e72f53298d4899db0f13762b26123
Bharitha09 <144610592+Bharitha09@users.noreply.github.com>
2023-10-11 22:20:34 -0500

    Create Stack.java

---
+++ Stack.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+   {
+       Stack<String>alist=new Stack<String>();
+       alist.push("Haritha");
+       alist.push("Viswa");
+       alist.push("Karun");
+       alist.push("Rishika");
+       alist.push("Sunny");
+       
+      
+       System.out.println(alist);
+       System.out.println("The pop out elements are"+alist.pop());
+      
+       
+   }
+}
