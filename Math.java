fc4d9080f34159e2ab93bb8c7ae61b5cad0f8134
Bharitha09 <144610592+Bharitha09@users.noreply.github.com>
2023-10-13 22:29:51 -0500

    Create Math.java

---
+++ Math.java
@@ -0,0 +1,19 @@
+class Main
+{
+    public static void main(String args[])
+     {
+
+       System.out.println("The PI Value is:"+Math.PI);
+       System.out.println("The Square Root  Value is:"+Math.sqrt(625));
+       System.out.println("The Minimum  Value is:"+Math.min(23,43));
+       System.out.println("The Maximum  Value is:"+Math.max(23,43));
+       System.out.println("The Absolute  Value is:"+Math.abs(-273));
+       System.out.println("The Ceil  Value is:"+Math.ceil(12.01));
+       System.out.println("The Floor Value is:"+Math.floor(12.99));
+       System.out.println("The Round  Value is:"+Math.round(16.55));
+       System.out.println("The Random  Value is:"+Math.random());
+       System.out.println("The log  Value is:"+Math.log(1));
+       System.out.println("The sin  Value is:"+Math.sin(45));
+
+      }
+}
